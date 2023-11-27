37d8eb2b7756982035120e93722ad943bf11fc6f
liweibingzhang <3486343935@qq.com>
2023-11-27 11:58:04 +0800

    代码格式化

--- 2023.11.25/Manager.cs
+++ 2023.11.25/Manager.cs
@@ -55,9 +55,9 @@ namespace DD_PJ
             conn.Open();
             //从数据库load信息
             Commodities = LoadCommoditiesInfo();
-            Sellers     = LoadSellersInfo();
-            Platforms   = LoadPlatformsInfo();
-            Users       = LoadUsersInfo();
+            Sellers = LoadSellersInfo();
+            Platforms = LoadPlatformsInfo();
+            Users = LoadUsersInfo();
         }
 
         /// <summary>
@@ -72,14 +72,17 @@ namespace DD_PJ
             switch (userType)
             {
                 case E_UserType.User:
-                    cmdStr = string.Format("INSERT INTO `user` VALUES(null, '{0}', '{1}', null, null, null)", name, password);
+                    cmdStr = string.Format("INSERT INTO `user` VALUES(null, '{0}', '{1}', null, null, null)", name,
+                        password);
                     break;
                 case E_UserType.Seller:
-                    cmdStr = string.Format("INSERT INTO `seller` VALUES(null, '{0}', '{1}', null, null, null)", name, password);
+                    cmdStr = string.Format("INSERT INTO `seller` VALUES(null, '{0}', '{1}', null, null, null)", name,
+                        password);
                     break;
                 default:
                     break;
             }
+
             cmd = new MySqlCommand(cmdStr, conn);
             cmd.ExecuteNonQuery();
             //更新注册表
@@ -114,15 +117,17 @@ namespace DD_PJ
             //    这关系到在写命令字符串时 一些字段需不需要用 单引号''包裹
             try
             {
-                using (MySqlConnection connection = new MySqlConnection("server=localhost;username=root;password=210906;database=PCS;"))
+                using (MySqlConnection connection =
+                       new MySqlConnection("server=localhost;username=root;password=210906;database=PCS;"))
                 {
                     connection.Open();
                     using (MySqlCommand command = new MySqlCommand())
                     {
                         command.Connection = connection;
                         // 构造插入语句
-                        command.CommandText = "INSERT INTO selling (commodity_id, seller_id, platform_id, produce_date, shelf_life, produce_address, price, description) " +
-                                              "VALUES (@commodity_id, @seller_id, @platform_id, @produce_date, @shelf_life, @produce_address, @price, @description)";
+                        command.CommandText =
+                            "INSERT INTO selling (commodity_id, seller_id, platform_id, produce_date, shelf_life, produce_address, price, description) " +
+                            "VALUES (@commodity_id, @seller_id, @platform_id, @produce_date, @shelf_life, @produce_address, @price, @description)";
 
                         // 添加参数
                         command.Parameters.AddWithValue("@commodity_id", sellingInfo.commodity_id);
@@ -137,77 +142,78 @@ namespace DD_PJ
                         command.ExecuteNonQuery();
                     }
                 }
+
                 Console.WriteLine("Publish successful!");
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
-            
         }
 
         /// <summary>
         /// 更新在售商品信息
         /// </summary>
         /// <param name="sellingInfo">新的在售商品信息</param>
-    public void UpdateSellingInfo(SellingInfo sellingInfo) 
-            //情景：商家对自己已发布的商品进行了修改，因此需要在数据库中实现对表selling的更新
-    //Tips：
-    // 1) 参数sellingInfo 是经过商家修改过后的在售商品信息，你要做的是用这些信息更新表selling中旧的信息
-    // 2) 更新需要用到的 参数sellingInfo 里的字段 可以 确保不为空
-    // 3）表selling 的定义见 我于2023.11.24日发送给你的压缩包 中的.sql文件
-    // 4) 类SellingInfo的定义请见 我于2023.11.25日上传至github 的SellingInfo.cs文件
-    // 5) 你在构建字符串时可能会用到string.Format()方法，请自行搜索其用法
-    // 6) 你可以参照此文件中其他函数的写法
-    // 7) 请注意类SellingInfo中的字段的类型 和 表selling中的字段的类型！！
-    //    这关系到在写命令字符串时 一些字段需不需要用 单引号''包裹
-    {
-        try
+        public void UpdateSellingInfo(SellingInfo sellingInfo)
         {
-            using (MySqlConnection connection = new MySqlConnection("server=localhost;username=root;password=210906;database=PCS;"))
+            //情景：商家对自己已发布的商品进行了修改，因此需要在数据库中实现对表selling的更新
+            //Tips：
+            // 1) 参数sellingInfo 是经过商家修改过后的在售商品信息，你要做的是用这些信息更新表selling中旧的信息
+            // 2) 更新需要用到的 参数sellingInfo 里的字段 可以 确保不为空
+            // 3）表selling 的定义见 我于2023.11.24日发送给你的压缩包 中的.sql文件
+            // 4) 类SellingInfo的定义请见 我于2023.11.25日上传至github 的SellingInfo.cs文件
+            // 5) 你在构建字符串时可能会用到string.Format()方法，请自行搜索其用法
+            // 6) 你可以参照此文件中其他函数的写法
+            // 7) 请注意类SellingInfo中的字段的类型 和 表selling中的字段的类型！！
+            //    这关系到在写命令字符串时 一些字段需不需要用 单引号''包裹
+            try
             {
-                connection.Open();
-                using (MySqlCommand command = new MySqlCommand())
+                using (MySqlConnection connection =
+                       new MySqlConnection("server=localhost;username=root;password=210906;database=PCS;"))
                 {
-                    command.Connection = connection;
-                    // 构造更新语句
-                    command.CommandText = "UPDATE selling " +
-                                      "SET produce_date = @produce_date, " +
-                                      "    shelf_life = @shelf_life, " +
-                                      "    produce_address = @produce_address, " +
-                                      "    price = @price, " +
-                                      "    description = @description " +
-                                      "WHERE commodity_id = @commodity_id AND seller_id = @seller_id AND platform_id = @platform_id";
-
-                    // 添加参数
-                    command.Parameters.AddWithValue("@produce_date", DateTime.Parse(sellingInfo.produce_date));
-                    command.Parameters.AddWithValue("@shelf_life", int.Parse(sellingInfo.shelf_life));
-                    command.Parameters.AddWithValue("@produce_address", sellingInfo.produce_address);
-                    command.Parameters.AddWithValue("@price", decimal.Parse(sellingInfo.price));
-                    command.Parameters.AddWithValue("@description", sellingInfo.description);
-                    command.Parameters.AddWithValue("@commodity_id", sellingInfo.commodity_id);
-                    command.Parameters.AddWithValue("@seller_id", sellingInfo.seller_id);
-                    command.Parameters.AddWithValue("@platform_id", sellingInfo.platform_id);
-
-                    // 执行更新操作
-                    int rowsAffected = command.ExecuteNonQuery();
-
-                    if (rowsAffected > 0)
+                    connection.Open();
+                    using (MySqlCommand command = new MySqlCommand())
                     {
-                        Console.WriteLine("Update successful!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("No rows updated. SellingInfo not found in the database.");
+                        command.Connection = connection;
+                        // 构造更新语句
+                        command.CommandText = "UPDATE selling " +
+                                              "SET produce_date = @produce_date, " +
+                                              "    shelf_life = @shelf_life, " +
+                                              "    produce_address = @produce_address, " +
+                                              "    price = @price, " +
+                                              "    description = @description " +
+                                              "WHERE commodity_id = @commodity_id AND seller_id = @seller_id AND platform_id = @platform_id";
+
+                        // 添加参数
+                        command.Parameters.AddWithValue("@produce_date", DateTime.Parse(sellingInfo.produce_date));
+                        command.Parameters.AddWithValue("@shelf_life", int.Parse(sellingInfo.shelf_life));
+                        command.Parameters.AddWithValue("@produce_address", sellingInfo.produce_address);
+                        command.Parameters.AddWithValue("@price", decimal.Parse(sellingInfo.price));
+                        command.Parameters.AddWithValue("@description", sellingInfo.description);
+                        command.Parameters.AddWithValue("@commodity_id", sellingInfo.commodity_id);
+                        command.Parameters.AddWithValue("@seller_id", sellingInfo.seller_id);
+                        command.Parameters.AddWithValue("@platform_id", sellingInfo.platform_id);
+
+                        // 执行更新操作
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            Console.WriteLine("Update successful!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No rows updated. SellingInfo not found in the database.");
+                        }
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error: " + ex.Message);
-        }
-    }
 
 
         /// <summary>
@@ -228,6 +234,7 @@ namespace DD_PJ
                     list.Add(SellingInfo.ToSellingInfo(reader));
                 }
             }
+
             return list;
         }
 
@@ -240,7 +247,7 @@ namespace DD_PJ
             string cmdStr = string.Format("UPDATE `seller` " +
                                           "SET `fullname` = '{0}', `password` = '{1}', `address` = '{2}', `description` = '{3}' " +
                                           "WHERE `ID` = {4}",
-                                          sellerInfo.fullname, sellerInfo.password, sellerInfo.address, sellerInfo.description, sellerInfo.id);
+                sellerInfo.fullname, sellerInfo.password, sellerInfo.address, sellerInfo.description, sellerInfo.id);
             cmd = new MySqlCommand(cmdStr, conn);
             cmd.ExecuteNonQuery();
         }
@@ -255,6 +262,7 @@ namespace DD_PJ
 
 
         #region 从数据库加载信息的方法
+
         /// <summary>
         /// 加载所有商品的信息
         /// </summary>
@@ -273,6 +281,7 @@ namespace DD_PJ
                     dic.Add(info.id, info);
                 }
             }
+
             return dic;
         }
 
@@ -294,6 +303,7 @@ namespace DD_PJ
                     dic.Add(info.id, info);
                 }
             }
+
             return dic;
         }
 
@@ -316,6 +326,7 @@ namespace DD_PJ
                     dic.Add(id, name);
                 }
             }
+
             return dic;
         }
 
@@ -337,8 +348,10 @@ namespace DD_PJ
                     dic.Add(info.id, info);
                 }
             }
+
             return dic;
         }
+
         #endregion
     }
 }
